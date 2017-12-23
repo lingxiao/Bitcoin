9ae391b4888585ae0f9273add612b00d881143eb
Xiao Ling <lingxiao@seas.upenn.edu>
2017-12-22 19:49:49 -0500

    add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Track price of bitcoins and backtest strategies
+
+
+API KEY   : L7QaKyIVT8BN1xRO
+API SECRET: RO8aTcFFI08l46fpwSbZgBs2wK6H0C9e
+
+
--- src/hello-coinbase.py
+++ src/hello-coinbase.py
@@ -2,13 +2,27 @@
 # Module  : None
 # Date    : December 22nd, 2017
 # Author  : Xiao Ling
+# To run  : run hello-coinbase.py
 ############################################################
 
 
 from coinbase.wallet.client import Client
 
 
-# client = Client(<api_key>, <api_secret>, api_version='YYYY-MM-DD')
+client        = Client( 'L7QaKyIVT8BN1xRO'
+	                  , 'RO8aTcFFI08l46fpwSbZgBs2wK6H0C9e'
+	                  , api_version='2017-12-22'
+	                  )
+currency_code = "USD"
+price         = client.get_spot_price(currency=currency_code)
+
+
+print('Current bitcoin price in %s: %s' % (currency_code, price.amount))
+
+
+
+
+
 
 
 
