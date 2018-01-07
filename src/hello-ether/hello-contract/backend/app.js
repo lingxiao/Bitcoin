7a8bf3d92b1ac83a1383663ded3a92718b09170a
Xiao Ling <lingxiao@seas.upenn.edu>
2018-01-07 12:43:24 -0500

    add

--- src/hello-ether/hello-contract/backend/app.js
+++ src/hello-ether/hello-contract/backend/app.js
@@ -6,15 +6,14 @@
 */
 
 const Web3    = require("web3");
+const path    = require("path");
 const pr      = require("../prelude");
-const path    = require("path")
 const express = require("express");
-
 
 var web3 = new Web3();
 
 // hard code it for now
-var dir = "/Users/lingxiao/Documents/Projects/Bitcoin/src/hello-ether/hello-contract/backend/"
+var template_dir = "/Users/lingxiao/Documents/Projects/Bitcoin/src/hello-ether/hello-contract/frontend"
 
 /**
     set up server using express
@@ -27,14 +26,17 @@ var dir = "/Users/lingxiao/Documents/Projects/Bitcoin/src/hello-ether/hello-cont
     what if I installed it here.. get it working here. and then transfer over?
     
     strategy:
-        1. serve simple html
-        2. serve more complex html and do some event listeners
+        1. serve simple html    -> done
+        2. serve more complex html and do some event listeners 
+            -> need a plug in the html
+            -> need to serve the html, and attach listeners to each plug
+            -> need to know the canonical way to factorize the code into differnt files
+
         3. connect Voting.js to this somehow, make sure the ether-blockchain is updating
         4. make a express boiler plate app and migrate to it
         5. integrate it into a bootstrap template for presentation sake
         6. Think about how it could be integrated into a mobile phone
 */
-
 // launch app
 var app = express()
 
@@ -46,14 +48,23 @@ app.get("/about"   , (req, res) => res.send("hello from about page"      ));
 app.get("/contact" , (req, res) => res.send("contact me at mememe@me.com"));
 
 // now we want to serve an xxx.html 
-app.get("/hello"  , function(req, res){
+app.get("/hello"  , function(req, res, next){
 
     // using absolute file path, need to get relative file path somehow
-    res.sendFile(dir + "hello.html");
+    res.sendFile(template_dir + "/hello.html");
+    next()
 
-})
+    // as a simple thing, should be able to redirect to a different place
+    // on loading
 
-// now serve a page with a button on it, and think about how to get events back
+}, function(req, res){
+
+    console.log("hello world from /hello route")
+});
+
+// now serve a page with fields in it, and using post to listen for events
+
+
 
 
 
