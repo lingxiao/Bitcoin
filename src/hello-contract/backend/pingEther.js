a86dfc26bbde0662064839922b22a8d0e427dae7
Xiao Ling <lingxiao@seas.upenn.edu>
2018-01-07 20:45:21 -0500

    add

--- src/hello-contract/backend/pingEther.js
+++ src/hello-contract/backend/pingEther.js
@@ -2,12 +2,14 @@
 	@file: index.js
 	@author: Xiao Ling <lingxiao@seas.upenn.edu>
 	@date: 1/2/2018
+
+	@Note: this runs with web3 0.20.1, do: npm install web3@0.20.1
 */
 
 // import modules
 var pr   = require("../lib/prelude");
-var Web3 = require("web3");
-var web3 = new Web3();
+var Web3 = require("web3");      // library that allow you to interact with remote ethereum code
+// var web3 = new Web3();
 
 /*
 	connecting to running ethereum node. Note the node must specify --rpcport 8545
@@ -23,21 +25,21 @@ var web3 = new Web3();
 
 */
 
-var ether_port = new Web3.providers.HttpProvider("http://localhost:8545");
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-web3.setProvider(ether_port);
+// web3.setProvider(ether_port);
 
-console.log("Ethereum-NodeJS App started ...");
-// console.log("Web3.js - API version: " + web3.version.api + " Node: " + web3.version.node.ethereum + "\n")
+// console.log("Ethereum-NodeJS App started ...");
+// // console.log("Web3.js - API version: " + web3.version.api + " Node: " + web3.version.node.ethereum + "\n")
 
-// get account address and print their account value
-// note we created two accounts earlier in the private blockchain
-var user_0 = web3.eth.accounts[0];
-var user_1 = web3.eth.accounts[1];
+// // get account address and print their account value
+// // note we created two accounts earlier in the private blockchain
+// var user_0 = web3.eth.accounts[0];
+// var user_1 = web3.eth.accounts[1];
 
-console.log("================= printing balance ====================")
+// console.log("================= printing balance ====================")
 
-console.log("account zero's balance is: " + web3.eth.getBalance(user_0)["c"][0])
-console.log("account one's balance is: " + web3.eth.getBalance(user_1)["c"][0])
+// console.log("account zero's balance is: " + web3.eth.getBalance(user_0)["c"][0])
+// console.log("account one's balance is: " + web3.eth.getBalance(user_1)["c"][0])
 
  	
