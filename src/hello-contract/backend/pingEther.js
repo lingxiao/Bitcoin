60ba56e1c960558aba4eedddeb2b914609477822
Xiao Ling <lingxiao@seas.upenn.edu>
2018-01-09 17:10:25 -0500

    add

--- src/hello-contract/backend/pingEther.js
+++ src/hello-contract/backend/pingEther.js
@@ -109,44 +109,37 @@ user_0_balance.then(function(tup){
 
 	other events include:
 		- syncing
-
+		- logs
 */ 
-var foo = web3.eth.subscribe("newBlockHeaders", function(err, result){
+var event_newBlockHeaders = web3.eth.subscribe("newBlockHeaders", function(err, result){
 	if (err){ console.log(err) }
-	else { console.log("result: ", result) }
+
+
+	/**
+		something like:
+
+		do 
+			accts <- web3.eth.getAccounts()
+			bal   <- web3.eth.getBalance(accts[0])
+			log ...
+	*/ 
+	else {
+
+		let acctPromise = web3.eth.getAccounts().then(function(accts){
+
+			let balance = web3.eth.getBalance(accts[0]).then(function(bal){
+
+				console.log("user: ", accts[0]);
+				console.log("balance: ", bal);
+
+			});
+
+		});
+	}
+
 });
 
 
 
-/*
-	connecting to running ethereum node. Note the node must specify --rpcport 8545
-	note: after moving the files around it's now no longer connecting
-	stategy: go through the whole thing from the top again:
-		-> new ethereum blockchain
-		-> new accounts
-		-> new port
-	conclusion: still not working ... 
-
-	it seems like things are async now... maybe it's because you upgraded things?
-	you're running a different version of web3 than what's in the blog
 
 
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-// web3.setProvider(ether_port);
-
-console.log("Ethereum-NodeJS App started ...");
-console.log("Web3.js - API version: " + web3.version.api + " Node: " + web3.version.node.ethereum + "\n")
-
-// get account address and print their account value
-// note we created two accounts earlier in the private blockchain
-var user_0 = web3.eth.accounts[0];
-var user_1 = web3.eth.accounts[1];
-
-console.log("================= printing balance ====================")
-
-console.log("account zero's balance is: " + web3.eth.getBalance(user_0)["c"][0])
-console.log("account one's balance is: " + web3.eth.getBalance(user_1)["c"][0])
-
-*/
- 	
